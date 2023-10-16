fb337e38a5fef5a4cd76652658692c752a018bb3
keke <934420809@qq.com>
2023-10-16 16:07:50 +0800

    test

---
+++ src/Demo01.java
@@ -0,0 +1,13 @@
+/**
+ * @BelongsProject: JavaSEStudy
+ * @BelongsPackage: test
+ * @Author: qzk
+ * @CreateTime: 2023/10/16 16:06
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
