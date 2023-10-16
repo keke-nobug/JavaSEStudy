432f38d4d07b43898e20490849397f8e865dfa43
keke <934420809@qq.com>
2023-10-16 16:10:17 +0800

    demo03

---
+++ src/Demo03.java
@@ -0,0 +1,13 @@
+/**
+ * @BelongsProject: JavaSEStudy
+ * @BelongsPackage: PACKAGE_NAME
+ * @Author: qzk
+ * @CreateTime: 2023/10/16 16:08
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class Demo03 {
+    public static void main(String[] args) {
+        System.out.println("456");
+    }
+}
