3f65cfb50816ba48d6f5a2c25a01a92997ffb4fc
keke <934420809@qq.com>
2024-01-16 16:39:03 +0800

    test001

---
+++ src/test/Test001.java
@@ -0,0 +1,15 @@
+package test;
+
+/**
+ * @BelongsProject: JavaSEStudy
+ * @BelongsPackage: test
+ * @Author: qzk
+ * @CreateTime: 2024/1/16 16:37
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class Test001 {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
