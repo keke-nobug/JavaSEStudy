461d5f3e0824c19ec70ea9368a263ed77161c48b
keke <934420809@qq.com>
2024-06-24 10:26:32 +0800

    添加LeetCode文件夹

--- src/LeetCode/HashTable/Lc242_ValidAnagram.java
+++ src/LeetCode/HashTable/Lc242_ValidAnagram.java
@@ -1,5 +1,8 @@
 package LeetCode.HashTable;
 
+import java.sql.SQLOutput;
+import java.util.HashMap;
+
 /**
  * @BelongsProject: JavaSEStudy
  * @BelongsPackage: LeetCode.HashTable
@@ -9,4 +12,24 @@ package LeetCode.HashTable;
  * @Version: 1.0
  */
 public class Lc242_ValidAnagram {
+    public static void main(String[] args) {
+
+        String s = "anagram";
+        String t = "nagaram";
+
+
+        HashMap<Character,Integer> window = new HashMap<>();
+        HashMap<Character,Integer> need = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            window.put(c, window.getOrDefault(c,0) + 1);
+        }
+        for (char c : t.toCharArray()) {
+            need.put(c, window.getOrDefault(c,0) + 1);
+        }
+
+        for (char c : s.toCharArray()) {
+            System.out.println(window.get(c) + "    " + need.get(c));
+            }
+    }
 }
