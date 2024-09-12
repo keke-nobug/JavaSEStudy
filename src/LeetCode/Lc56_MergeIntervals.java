f83525f7ee6da159731657481122136d6fa58c3e
keke <934420809@qq.com>
2024-09-12 11:19:00 +0800

    Lc56

--- src/LeetCode/Lc56_MergeIntervals.java
+++ src/LeetCode/Lc56_MergeIntervals.java
@@ -1,6 +1,9 @@
 package LeetCode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * @BelongsProject: JavaSEStudy
@@ -14,27 +17,30 @@ public class Lc56_MergeIntervals {
     public static void main(String[] args) {
         int[][] intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
 
-        System.out.println(merge(intervals)[0][1]);
+        System.out.println(merge(intervals)[2][1]);
     }
     public static int[][] merge(int[][] intervals) {
         int n = intervals.length;
-
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i < n - 1; i++) {
-            if (intervals[i][1] >= intervals[i + 1][0]) {
-                list.add(intervals[i][0]);
-                list.add(intervals[i+1][1]);
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0]; // 左端点从小到大排序，左减右 小到大正序
             }
+        });
 
-            list.add(intervals[i][0]);
-            list.add(intervals[i][1]);
-        }
+        // 存放合并后的结果
+        List<int[]> ans = new ArrayList<>();
 
-        int[][] ans = new int[list.size()/2][2];
-        for (int i = 0; i < list.size() / 2; i++) {
-            intervals[i][0] = list.get(i);
-            intervals[i][1] = list.get(i+1);
+        for (int[] p : intervals) {
+            int m = ans.size(); // 初始是0
+            if (m > 0 && ans.get(m - 1)[1] >= p[0]) { // 比较ans右端点和p的左端点，如果左>=右
+                ans.get(m - 1)[1] =  Math.max(ans.get(m - 1)[1], p[1]); // 更新右端点最大值
+            }else { // 否则直接加入结果集
+                ans.add(p);
+            }
         }
-        return ans;
+        // 转成二维数组
+        return ans.toArray(new int[ans.size()][]);
+
     }
 }
